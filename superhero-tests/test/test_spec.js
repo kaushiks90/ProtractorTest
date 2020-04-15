15bc578c1deb03b02ffdc9025d2fa013ca7e2088
kaushiks90 <kaushiks90@gmail.com>
2020-04-15 09:42:38 +0530

    first test

--- superhero-tests/test/test_spec.js
+++ superhero-tests/test/test_spec.js
@@ -1,7 +1,24 @@
 describe('super Hero Page', function () {
+  beforeEach(function () {});
+  afterEach(function () {});
   it('should load the correct url', function () {
     browser.ignoreSynchronization = true;
-    browser.get('file:///C:/Users/Koushik/Desktop/Angular/Protractor/superhero/index.html');
-    browser.sleep(10000);
+    //Open Url
+    browser.get(
+      'file:///C:/Users/Koushik/Desktop/Angular/Protractor/superhero/index.html'
+    );
+    //Enter Text into the field
+    element(by.id('loginEmail')).sendKeys('Just Testing');
+    element(by.id('loginPassword')).sendKeys('password');
+
+    //Verify stuff
+    expect(element(by.id('login-title')).getText()).toEqual(
+      'Welcome. Please Log In.'
+    );
+    expect(element(by.id('loginEmail')).getAttribute('value')).toEqual(
+      'Just Testing'
+    );
+
+    browser.sleep(5000);
   });
 });
