60de8e453da72df5adfe36247b7ec111a9df9756
kaushiks90 <kaushiks90@gmail.com>
2020-04-18 19:19:02 +0530

    Added console log capture and  screen shot capture

--- superhero-tests/conf.js
+++ superhero-tests/conf.js
@@ -1,3 +1,11 @@
+let SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+var HtmlScreenshotReporter = require('protractor-jasmine2-screenshot-reporter');
+
+var reporter = new HtmlScreenshotReporter({
+    dest: 'target/screenshots',
+    filename: 'my-report.html',
+    captureonlyFailedSpecs: true
+});
 exports.config = {
     framework: 'jasmine2',
 
@@ -8,15 +16,47 @@ exports.config = {
     },
 
     specs: [
-        // './test/login_spec.js',
-        //'./test/header_spec.js'
-        // './test/nav_spec.js'
-        // './test/roster_spec.js'
-        './test/vote_spec.js'
+        './test/login_spec.js',
+        './test/header_spec.js',
+        './test/nav_spec.js',
+        //'./test/roster_spec.js',
+        //'./test/vote_spec.js'
+        //'./test/*_spec.js'
     ],
 
     jasmineNodeOpts: {
-        showColors: true
+        showColors: true,
+        silent: true,
+        //defaultTimeoutInterval: 360000,
+        print: function () {}
+    },
+
+    // Setup the report before any tests start
+    beforeLaunch: function () {
+        return new Promise(function (resolve) {
+            reporter.beforeLaunch(resolve);
+        });
+    },
+
+    // Close the report after all tests finish
+    afterLaunch: function (exitCode) {
+        return new Promise(function (resolve) {
+            reporter.afterLaunch(resolve.bind(this, exitCode));
+        });
+    },
+
+    logLevel: 'WARN',
+    onPrepare: function () {
+        jasmine.getEnv().addReporter(reporter);
+
+        jasmine.getEnv().addReporter(new SpecReporter({
+            spec: {
+                displayStacktrace: true
+            },
+            summary: {
+                displayDuration: false
+            }
+        }));
     }
 
 }
